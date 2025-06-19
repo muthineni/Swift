b1d005ed60581fbcefbaabfe01c7a6c350fcfcb4
Sridhar <Sridhar-rao.muthineni@capgemini.com>
2025-06-19 17:15:16 -0400

    updates

--- UsingPackage/UsingPackage/ContentView.swift
+++ UsingPackage/UsingPackage/ContentView.swift
@@ -1,23 +1,78 @@
 //
 //  ContentView.swift
-//  UsingPackage
+//  usingPM
 //
-//  Created by Muthineni, Sridhar on 9/29/21.
+//  Created by Muthineni, Sridhar on 6/3/25.
 //
 
 import SwiftUI
-import FirstPackage
+import NetworkManager
 
-struct ContentView: View {
-    
-    var body: some View {
-        Text(FirstPackage().sayPackageHello())
-            .padding()
+
+struct DeviceResponse: Codable {
+    let id, name: String
+    let data: DataClass?
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let dataColor, dataCapacity: String?
+    let capacityGB: Int?
+    let dataPrice: Double?
+    let dataGeneration: String?
+    let year: Int?
+    let cpuModel, hardDiskSize, strapColour, caseSize: String?
+    let color, description, capacity: String?
+    let screenSize: Double?
+    let generation, price: String?
+
+    enum CodingKeys: String, CodingKey {
+        case dataColor = "color"
+        case dataCapacity = "capacity"
+        case capacityGB = "capacity GB"
+        case dataPrice = "price"
+        case dataGeneration = "generation"
+        case year
+        case cpuModel = "CPU model"
+        case hardDiskSize = "Hard disk size"
+        case strapColour = "Strap Colour"
+        case caseSize = "Case Size"
+        case color = "Color"
+        case description = "Description"
+        case capacity = "Capacity"
+        case screenSize = "Screen size"
+        case generation = "Generation"
+        case price = "Price"
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            
+            Button(
+                "click it",
+                action: {
+                    NetworkServices().get(endPoint: "https://api.restful-api.dev/objects", withHeaders: nil) { (response: NetworkServiceResponse<[DeviceResponse]>) in
+                        if response.status == .success {
+//                            guard let deviceName = response.data?.name[0]
+//
+//                            else { return }
+//                            guard let color = response.data?.data?.color else { return }
+                            print("####### items", response.data)
+                        }
+                    }
+                })
+        }}
+}
+
+#Preview {
+    ContentView()
 }
